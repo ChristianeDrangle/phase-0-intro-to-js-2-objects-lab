1b7a1c5e8e4bf17206dbeb057685eb60d1d92be6
Christiane Drangle <positivitee@proton.me>
2023-03-11 09:43:24 -0500

    passing Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name: "Alicia", 
+    streetAddress: "Cripple Creek",
+}
+
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+    employee [key] = value;
+    return employee;
+}
+
+
+function deleteFromEmployeeByKey(employee,key) {
+    const newEmployee = {employee};
+    employee[key] = `Sam`;
+    return newEmployee;
+}
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key]
+    return employee
+}
+
